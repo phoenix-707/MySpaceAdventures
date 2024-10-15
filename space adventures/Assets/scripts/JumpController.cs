2c149bdf5e9840c120274c21940b33906fee9463
phoenix-707 <152482919+phoenix-707@users.noreply.github.com>
2024-10-15 22:47:27 +1000

    FirstCommit

---
+++ space adventures/Assets/scripts/JumpController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JumpController : MonoBehaviour
+{
+    public float gravity = 9.8f;
+    public float jumpForce;
+    private float _fallVelocity = 1;
+    private CharacterController _CharacterController;
+    void Start()
+    {
+        _CharacterController = GetComponent<CharacterController>();
+    }
+
+    void FixedUpdate()
+    {
+        _fallVelocity += gravity * Time.fixedDeltaTime;
+        _CharacterController.Move(Vector3.down * _fallVelocity * Time.fixedDeltaTime);
+        if (_CharacterController.isGrounded)
+        {
+            _fallVelocity = 0;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            _fallVelocity = -jumpForce;
+        }
+    }
+}
---
+++ space adventures/Assets/scripts/planetRotate.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class planetRotate : MonoBehaviour
+{
+    public float rotationSpeed;
+    private CharacterController _controller;
+    void Start()
+    {
+        _controller = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        transform.Rotate(0, 1, 0);
+    }
+}
